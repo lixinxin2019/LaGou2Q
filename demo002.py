1c593a06faabea368e1a21d29679f91804fa0536
lixinxin <lixinxin@ktjr.com>
2020-06-12 13:30:30 +0800

    python 实战（二）使用yaml来管理实例属性

--- demo002.py
+++
@@ -1,8 +0,0 @@
-import requests
-# r = requests.get("http://www.baidu.com")
-# r.encoding = "utf-8"
-
-r = requests.post( 'https://service-trunk.xiaoyuncai.com/api/login',data = {"key":"value"})
-print(r.text)
-
-
