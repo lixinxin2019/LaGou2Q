8962b4088eec5fcdd8d08aef73d38e7283591542
lixinxin <lixinxin@ktjr.com>
2020-06-12 13:33:32 +0800

    python 实战（二）使用yaml来管理实例属性

--- homework002.py
+++ homework002.py
@@ -1,6 +1,9 @@
 # 课后作业：自己写一个面向对象的例子
 # 描述：
 # 创建一个类（Animal）【动物类】，类里有属性（名称，颜色，年龄，性别），类方法（会叫，会跑）
+import yaml
+
+
 class Animal:
 
     def __init__(self, name, color, age, gender):
@@ -35,7 +38,7 @@ class Cat(Animal):
     def shout(self):
         print(f"{self.name} 会喵喵叫~")
 
-    #打印猫猫的信息
+    # 打印猫猫的信息
     def info(self):
         print(f"猫猫的名字是：{self.name},颜色是：{self.color},年龄是：{self.age},性别是：{self.gender},毛发是：{self.hair},捉到了老鼠")
 
@@ -58,22 +61,38 @@ class Dog(Animal):
     # - 复写父类的【会叫】的方法，改成【汪汪叫】
     def shout(self):
         print(f"{self.name} 会汪汪叫！")
+
     # - 打印狗狗的信息
     def info(self):
         print(f"狗狗的名字是：{self.name},颜色是：{self.color},年龄是：{self.age},性别是：{self.gender},毛发是：{self.hair}")
 
 
 if __name__ == '__main__':
+    #调用data.yaml中的数据：
+    with open("data.yaml") as f:
+        datas = yaml.safe_load(f)
+        print(datas)
+    mycat = datas["mycat"]
+    mydog = datas["mydog"]
+
     # 创建一个猫猫实例
-    cat = Cat("小黑猫", "黑色", 1, "母猫")
+    #cat = Cat("小黑猫", "黑色", 1, "母猫")
+
+    #创建一个猫猫实例，使用data.yaml中的数据管理实例的属性
+    cat = Cat(mycat["name"], mycat["color"], mycat["age"], mycat["gender"])
+
     # - 调用捉老鼠的方法
     cat.catch_mouse()
 
     # - 打印【猫猫的姓名，颜色，年龄，性别，毛发，捉到了老鼠】。
     cat.info()
 
+    #===========================================================
     # 创建一个狗狗实例
-    dog = Dog("阿黄", "黄色", 1, "母狗")
+    #dog = Dog("阿黄", "黄色", 1, "母狗")
+
+    # 创建一个狗狗实例，使用data.yaml中的数据管理实例的属性
+    dog = Dog(mydog["name"], mydog["color"], mydog["age"], mydog["gender"])
 
     # - 调用【会看家】的方法
     dog.outstanding()
